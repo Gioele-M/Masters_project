c17db599e265c026648b70bfc159bbd506a29c69
Gioele <gioelemontis97@gmail.com>
2021-07-19 12:47:02 +0100

    Using SeqIO to_dict instead of next

--- starting_ex/fasta_parser.py
+++ starting_ex/fasta_parser.py
@@ -1,12 +1,22 @@
-from Bio.SeqIO import parse 
+from Bio.SeqIO import parse, to_dict 
 from Bio.SeqRecord import SeqRecord 
 from Bio.Seq import transcribe 
 from Bio.Seq import Seq 
+from Bio.Blast import NCBIWWW
 
 file = open("human_mx1.fasta") 
 
 #get the first itterable with next() 
-sequence_record = next(parse(file, 'fasta'))
+#sequence_record = next(parse(file, 'fasta'))
+
+sequence_record_dict = to_dict(parse(file, 'fasta'))
+
+#convert FIRST (0) dictionary keys to a list 
+sequence_record = sequence_record_dict[list(sequence_record_dict)[0]]
+
+
+
+
 
 #print(sequence_record.id)
 
@@ -17,7 +27,7 @@ protein_sequence = rna_sequence.translate()
 
 print(protein_sequence)
 
-
+#result_handle = NCBIWWW.qblast("blastn", "nt", sequence_record.seq)
 
 
 
