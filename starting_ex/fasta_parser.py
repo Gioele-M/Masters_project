f7f192e0901e2d0d05da0c1395fc5f445e5a44a7
Gioele <gioelemontis97@gmail.com>
2021-07-26 11:53:22 +0100

    work of 2nd week

--- starting_ex/fasta_parser.py
+++ starting_ex/fasta_parser.py
@@ -1,8 +1,12 @@
+import pandas as pd
+import numpy as np
 from Bio.SeqIO import parse, to_dict 
 from Bio.SeqRecord import SeqRecord 
 from Bio.Seq import transcribe 
 from Bio.Seq import Seq 
 from Bio.Blast import NCBIWWW
+from Bio import SearchIO
+
 
 file = open("human_mx1.fas") 
 
@@ -17,7 +21,6 @@ sequence_record = sequence_record_dict[list(sequence_record_dict)[0]]
 #print(sequence_record.id)
 
 rna_sequence = transcribe(sequence_record.seq)
-rna_sequence.back_transcribe()
 
 protein_sequence = rna_sequence.translate()
 
@@ -31,31 +34,3 @@ print(blastn_results)
 
 
 
-
-
-
-
-
-
-
-#old bits
-""" records = parse(file, 'fasta') 
-for record in records:
-    print("Id: %s" % record.id) 
-    print("Name: %s" % record.name) 
-    print("Description: %s" % record.description) 
-    print("Annotations: %s" % record.annotations) 
-    print("Sequence Data: %s" % record.seq)
-    dna_seq = record.seq
-
-#print(dna_seq)
-
-rna_seq = transcribe(dna_seq)
-rna_seq.back_transcribe()
-
-#print(rna_seq)
-
-protein_seq = rna_seq.translate()
-
-print(protein_seq)
- """
---
+++ starting_ex/fasta_parser2.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import numpy as np
+from Bio import SeqIO, SeqRecord, Seq, SearchIO
+from Bio.Blast import NCBIWWW
+
+def mk_sequence_record(file, format='fasta'):
+    seq_record_dictionary = SeqIO.to_dict(SeqIO.parse(file, format)) #maybe best with read instead of parse
+    sequence_record = seq_record_dictionary[list(seq_record_dictionary)[0]]
+    return sequence_record
+
+
+def covert_to_protein(seq):
+    rna = Seq.transcribe(seq.seq)
+    protein = rna.translate(to_stop=True)
+    return protein #returns Seq object
+
+
+def blast_search_xml_save(sequence, filename, type='blastn', database = 'nt'):
+    handler = NCBIWWW.qblast(type, database, sequence.seq)
+    storer = handler.read()
+    with open(filename.xml, 'w') as savefile:
+        savefile.write(storer)
+
+
+def single_result_xml_read_to_pd_dict(filename):
+    result = SearchIO.read(filename, 'blast-xml')
+    dictionary = SearchIO.to_dict(result)
+    df = pd.DataFrame.from_dict(dictionary)
+    return df
+
+
+
+seq = mk_sequence_record("human_mx1.fas")
+blast_search_xml_save(seq, 'try1')
+dataframe = single_result_xml_read_to_pd_dict('try1')
+print(dataframe)
