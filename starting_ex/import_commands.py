3be999a390eb826d0d6b52ca41f61e0589b1e24b
Gioele <gioelemontis97@gmail.com>
2021-09-19 01:15:25 +0100

    Functioning command importing

--- starting_ex/import_commands.py
+++ starting_ex/import_commands.py
@@ -1,10 +1,12 @@
+import argparse
+
 #USER INPUTS
 filename = 'sry_gene.fasta' #'human_mx1.fas'
-taxid_list = [] #['9592']
 mafft_directory = r'/Users/Gioele/miniconda3/bin/mafft'
 email = 'A.N.Other@example.com'
 output_name = 'run1'
 
+taxid_list = [] #['9592']
 
 #OPTIONAL USER INPUTS
 local_query = False
@@ -17,5 +19,42 @@ len_threshold = 50
 identity_threshold = 50
 
 sequences_per_taxon = 3
-import_aa_sequence = True
+import_aa_sequence = False
 query_protein = 'sry_protein.fasta'
+
+
+type_of_molecule = 'nucleotide'
+
+
+
+
+
+command_parser = argparse.ArgumentParser(description='Run your BLAST and obtain MSA and phylogenetic tree')
+
+command_parser.add_argument('input_file')
+command_parser.add_argument('mafft_directory')
+command_parser.add_argument('email')
+command_parser.add_argument('output_name')
+
+#Taxid list as optional list
+command_parser.add_argument('-x', '--taxid_list', nargs='*')
+
+#optional argument
+command_parser.add_argument('-l', '--local_query', action='store_true', default=False) #action='store_true' -> returns true if flag is called 
+command_parser.add_argument('-t', '--threading', action='store_true', default=False)
+
+command_parser.add_argument('-e', '--evalue_threshold', type=float, default=10**-10)
+command_parser.add_argument('-n', '--length_threshold', type=float, default=50.0)
+command_parser.add_argument('-i', '--identity_threshold', type=float, default=50.0)
+
+command_parser.add_argument('-s', '--sequences_per_taxon', type=int, default=3)
+command_parser.add_argument('-p', '--import_protein', action='store_true', default=False)
+command_parser.add_argument('-q', '--query_protein', help='Filename')
+
+
+#Optional argument from list of options
+command_parser.add_argument('-m', '--molecule_type', choices=['nucleotide', 'aminoacid', 'entry'],default='nucleotide')
+
+args = command_parser.parse_args()
+
+print(f'input {args}')
